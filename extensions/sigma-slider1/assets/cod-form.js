99c1eed8e68458e797de5e3f69135c57cd99d7e4
zohaibsigma <zohaib@techsigma.us>
2024-05-22 15:29:09 +0500

    add js file

---
+++ extensions/sigma-slider1/assets/cod-form.js
@@ -0,0 +1,6 @@
+let body_main = document.querySelector("body");
+
+console.log("body_main", body_main);
+
+let code_btn = document.querySelector(".cod-btn button");
+console.log("code_btn", code_btn);
