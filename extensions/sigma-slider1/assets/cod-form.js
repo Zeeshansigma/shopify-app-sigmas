6de2d063b55f5b056a7517f26d6cd68d1ddd83b8
zohaibsigma <zohaib@techsigma.us>
2024-05-23 18:11:57 +0500

    add form data

--- extensions/sigma-slider1/assets/cod-form.js
+++ extensions/sigma-slider1/assets/cod-form.js
@@ -1,3 +1,41 @@
+// theme quantity 
+let theme_quantity = document.querySelector("quantity-input input");
+console.log("theme_quantity", theme_quantity);
+const priceElements = document.querySelectorAll('.product_price_cod');
+let badge = document.querySelector(".quantity_cod_badge p");
+console.log( "badge", badge.textContent)
+function extractPrice(text) {
+    return parseFloat(text.replace(/[^0-9.-]+/g, ""));
+}
+const basePrices = Array.from(priceElements).map(priceElement => extractPrice(priceElement.textContent));
+function updatePrices() {
+    let quantity = parseInt(theme_quantity.value, 10);
+    if (isNaN(quantity) || quantity < 1) {
+        quantity = 1;
+        theme_quantity.value = 1;
+    }
+    priceElements.forEach((priceElement, index) => {
+        const newPrice = basePrices[index] * quantity;
+        priceElement.textContent = `$${newPrice.toFixed(2)}`; 
+    });
+    logCurrentValue();
+}
+theme_quantity.addEventListener('change', () => {
+    let currentValue = parseInt(theme_quantity.value, 10);
+    if (isNaN(currentValue) || currentValue < 1) {
+        theme_quantity.value = 1;
+    }
+    updatePrices();
+     badge.textContent = theme_quantity.value;
+});
+function logCurrentValue() {
+    console.log(theme_quantity.value);
+}
+updatePrices();
+logCurrentValue();
+    // end
+
+
 let cod_btn = document.querySelector(".cod-btn button");
 let cod_form = document.querySelector(".cod-form-layer-main");
 let cod_cross = document.querySelector(".cross");
@@ -12,3 +50,5 @@ cod_btn.addEventListener("click", function() {
 cod_cross.addEventListener("click", function() {
     newDiv.style.setProperty('display', 'none', 'important');
 });
+
+
